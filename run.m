67e20626936c6bcb560b78adf6d7f81d724c02ac
Patrick Donelan <donelan.patrick17@gmail.com>
2026-02-16 21:37:01 -0500

    normalize control input, increase number of collocation intervals, mark 'crawl' phase as done

--- run.m
+++ run.m
@@ -1,5 +1,5 @@
 % number of collocation intervals
-N = 15;
+N = 21;
 
 % boundary conditions [m, m, rad]
 % orientation = 0 is north, orientation = pi/2 is east
@@ -38,21 +38,21 @@ cost = @(z) compute_cost(z, N);
 
 % constraint function (dynamics and boundary conditions)
 % z = [x (3*(N+1)); u (2*(N+1)); tauf (1)]
-constraints = @(z) nonlcon(z, x0, xf, current_field, wheelbase, N);
+constraints = @(z) nonlcon(z, x0, xf, current_field, wheelbase, max_speed, max_steer, N);
 
 % initial guess (straight line, constant controls)
 z0 = initialize_guess(N, x0, xf);
 
 % decision variable bounds
-lb = [repmat([-Inf; -Inf; -pi], N+1, 1); repmat([-max_speed; -max_steer], N+1, 1); 0];
-ub = [repmat([ Inf;  Inf;  pi], N+1, 1); repmat([ max_speed;  max_steer], N+1, 1); Inf];
+lb = [repmat([-Inf; -Inf; -pi], N+1, 1); repmat([-1; -1], N+1, 1); 0];
+ub = [repmat([ Inf;  Inf;  pi], N+1, 1); repmat([ 1;  1], N+1, 1); Inf];
 
 % optimize
 zopt = fmincon(cost, z0, [], [], [], [], lb, ub, constraints);
 
 % plot results
 [x, u, tauf] = extract_state(zopt, N);
-lpm.plot_results(x, u, tauf, x0, [xf; nan], @(tau, x, u) system_dynamics(tau, x, u, current_field, wheelbase));
+lpm.plot_results(x, u, tauf, x0, [xf; nan], @(tau, x, u) system_dynamics(tau, x, u, current_field, wheelbase, max_speed, max_steer));
 
 figure('Theme', 'light');
 hold on;
@@ -62,7 +62,6 @@ margin               = 0.2;
 [x2_min, x2_max]     = bounds(x(2,:));
 x1_pad               = margin * (x1_max - x1_min);
 x2_pad               = margin * (x2_max - x2_min);
-
 x1                   = linspace(x1_min - x1_pad, x1_max + x1_pad, 25);
 x2                   = linspace(x2_min - x2_pad, x2_max + x2_pad, 25);
 [X1, X2]             = meshgrid(x1, x2);
@@ -73,7 +72,7 @@ C1                   = C(1,:);
 C2                   = C(2,:);
 quiver(X1, X2, C1, C2, 'DisplayName', 'current');
 
-[~, x_s] = lpm.simulate(x0, u, tauf, @(tau, x, u) system_dynamics(tau, x, u, current_field, wheelbase));
+[~, x_s] = lpm.simulate(x0, u, tauf, @(tau, x, u) system_dynamics(tau, x, u, current_field, wheelbase, max_speed, max_steer));
 
 plot(x0(1), x0(2), 'k^', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'initial position');
 plot(xf(1), xf(2), 'kd', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'goal position');
@@ -113,8 +112,8 @@ s   = 0.5 * (tau + 1);
 xy  = x0(1:2) * (1 - s) + xf * s;
 x   = [xy; theta0 * ones(1, N+1)];
 
-% constant speed, zero steering
-u = [ones(1, N+1); zeros(1, N+1)];
+% constant half speed, zero steering (normalized)
+u = [0.5 * ones(1, N+1); zeros(1, N+1)];
 
 z0 = [x(:); u(:); tauf];
 end
@@ -130,7 +129,7 @@ tauf = z(end);
 end
 
 %% compute constraints
-function [c, ceq] = nonlcon(z, x0, xf, current_field, wheelbase, N)
+function [c, ceq] = nonlcon(z, x0, xf, current_field, wheelbase, max_speed, max_steer, N)
 % extract state variables
 [x, u, tauf] = extract_state(z, N);
 
@@ -146,7 +145,7 @@ ceq = zeros(3 * (N + 1) + 5, 1);
 
 % compute equality constraints due to system dynamics
 ic        = 3 * (N + 1);
-ceq(1:ic) = reshape(x * D' - 0.5 * tauf * system_dynamics(tau, x, u, current_field, wheelbase), [], 1);
+ceq(1:ic) = reshape(x * D' - 0.5 * tauf * system_dynamics(tau, x, u, current_field, wheelbase, max_speed, max_steer), [], 1);
 
 % compute equality constraints due to boundary conditions
 ceq(ic+1:ic+3) = x(:,1) - x0;
@@ -154,10 +153,10 @@ ceq(ic+4:ic+5) = x(1:2,end) - xf;
 end
 
 %% system dynamics of kinematic vehicle with current
-function [xdot] = system_dynamics(~, x, u, current_field, wheelbase)
+function [xdot] = system_dynamics(~, x, u, current_field, wheelbase, max_speed, max_steer)
 theta = x(3,:);
-v     = u(1,:);
-delta = u(2,:);
+v     = u(1,:) * max_speed;
+delta = u(2,:) * max_steer;
 c     = current_field(x(1,:), x(2,:));
 
 xdot = [v .* sin(theta) + c(1,:);
